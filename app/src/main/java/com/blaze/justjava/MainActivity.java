4a8ac0ce27569ca96e7d7512666d104cdf14466a
Vijay Kumar Singh <vks14022000@gmail.com>
2021-02-17 21:56:27 +0530

    Added Order Summary and optimized the code

--- app/src/main/java/com/blaze/justjava/MainActivity.java
+++ app/src/main/java/com/blaze/justjava/MainActivity.java
@@ -26,14 +26,28 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        displayMessage("Total: ₹" + price + "\nThank you!");
+        int price = calculatePrice();
+        createOrderSummary(price);
+    }
+
+    /**
+     * This method Calculates the price of the order and @returns total price.
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
+    /**
+     * This method displays the order summary on the screen.
+     */
+    private void createOrderSummary(int price) {
+        displayMessage("Name: Sam \nQuantity: " + quantity + "\nTotal: ₹" + price + "\nThank you!");
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_tv);
         quantityTextView.setText("" + number);
     }
@@ -42,21 +56,21 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        display(++quantity);
+        displayQuantity(++quantity);
     }
 
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        display(--quantity);
+        displayQuantity(--quantity);
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = findViewById(R.id.price_tv);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_tv);
+        orderSummaryTextView.setText(message);
     }
 }
